b46b198da5882f7eecfa3e741f1661676c2aaba5
Alex <eshobee@gmail.com>
2017-11-21 20:24:00 +0300

    "hello from electron!"

--- client/app.js
+++ client/app.js
@@ -1,5 +1,39 @@
 'use strict';
 
 const logger = require('./src/logger');
+const path = require('path');
+const url = require('url');
+const {app, BrowserWindow} = require('electron');
 
-logger.info("Hello, world!");
+let win;
+
+function createWindow() {
+    let win = new BrowserWindow({width: 400, height: 320});
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'templates', 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    win.on('close', () => {
+        logger.debug("Exit -> close window");
+        win = null;
+    });
+
+    win.once('ready-to-show', () => {
+        logger.debug("Ready -> showing window");
+        win.show();
+    });
+}
+
+app.on('ready', createWindow);
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+app.on('activate', () => {
+    if (win === null) {
+        createWindow();
+    }
+});
