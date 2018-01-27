0caef3a83053371783ff7b664fd4e3fdcc151c2b
Alex <eshobee@gmail.com>
2018-01-28 00:48:47 +0300

    [engine] update utils: add and configure global jinja2 environment

--- engine/utils/__init__.py
+++ engine/utils/__init__.py
@@ -1,6 +1,21 @@
 import os
+import sys
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
 import engine
 
 PATHS = {'root': os.path.dirname(engine.__file__)}
 PATHS['static'] = os.path.join(PATHS['root'], "static")
 PATHS['templ'] = os.path.join(PATHS['root'], "templates")
+
+FILE_ENCODING = "utf-8"
+
+ENV = Environment(
+    loader=FileSystemLoader(PATHS['templ'], encoding=FILE_ENCODING),
+    autoescape=select_autoescape(enabled_extensions=(), default=False),
+    trim_blocks=True,
+    lstrip_blocks=True,
+    newline_sequence="\r\n" if "win" in sys.platform else "\n",
+    keep_trailing_newline=True,
+    auto_reload=False
+)
