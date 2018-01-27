2c4c58a31f25cee47d69914abbe41b9df70a4f22
Alex <eshobee@gmail.com>
2018-01-28 00:49:28 +0300

    [engine] update rendering, add realisation for sdc

--- engine/utils/render.py
+++ engine/utils/render.py
@@ -1,56 +1,60 @@
-import os
-from functools import partial
-from jinja2 import Environment, FileSystemLoader
+""" Rendering interface """
 
-from engine import ROOT_DIR
+import logging
+from typing import Iterator
+
+from jinja2.environment import Template
+
 from engine.utils.decorators import default_args_for_render, load_template
-
-TEMPL_ENV = Environment()
-TEMPL_ENV.loader = FileSystemLoader(os.path.join(ROOT_DIR, "templates"))
-
-load_template = partial(load_template, env=TEMPL_ENV)
+from engine.utils.log import log
 
 
 class Render(object):
     """ Collection of renders for templates """
 
     @staticmethod
-    @load_template("qpf")
+    @log()
+    def _render(template: Template, **kwargs) -> Iterator[str]:
+        logging.debug("Rendering '%s' template...", template.filename)
+        return template.generate(**kwargs)
+
+    @staticmethod
+    @load_template("qpf.jinja")
     @default_args_for_render
     def qpf(meta_info: dict=None,
             revisions: dict=None,
-            **kwargs) -> str:
-        return kwargs.get('template').render(
+            **kwargs) -> Iterator[str]:
+        """ Template rendering interface for .qpf files """
+        return Render._render(
             meta_info=meta_info,
             revisions=revisions,
             **kwargs
         )
 
     @staticmethod
-    @load_template("qsf")
+    @load_template("qsf.jinja")
     @default_args_for_render
     def qsf(global_assignments: dict=None,
             user_assignments: dict=None,
-            **kwargs) -> str:
-        return kwargs.get('template').render(
+            **kwargs) -> Iterator[str]:
+        """ Template rendering interface for .qsf files """
+        return Render._render(
             global_assignments=global_assignments,
             user_assignments=user_assignments,
             **kwargs
         )
 
-    # TODO: legacy?
+    # TODO: not useful?
     @staticmethod
-    @load_template("sdc")
+    @load_template("sdc.jinja")
     @default_args_for_render
-    def sdc(**kwargs) -> NotImplemented:
-        """ Throws exception """
-        raise NotImplementedError
+    def sdc(**kwargs) -> Iterator[str]:
+        """ Template rendering interface for .sdc files """
+        return Render._render(**kwargs)
 
     @staticmethod
-    @load_template("v")
+    @load_template("v.jinja")
     @default_args_for_render
-    def v(project_name: str, **kwargs) -> str:
-        return kwargs.get('template').render(
-            project_name=project_name,
-            **kwargs
-        )
+    def v(project_name: str, **kwargs) -> Iterator[str]:
+        """ Template rendering interface for .v files """
+        return Render._render(project_name=project_name, **kwargs)
