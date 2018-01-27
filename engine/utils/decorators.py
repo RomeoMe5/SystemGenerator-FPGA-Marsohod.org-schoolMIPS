064cbca6002c00dee7cd0ce14f982dc4c51bd748
Alex <eshobee@gmail.com>
2018-01-28 00:48:09 +0300

    [engine] update decorators

--- engine/utils/decorators.py
+++ engine/utils/decorators.py
@@ -1,10 +1,16 @@
-from collections import Callable
+import logging
+import os
 from functools import wraps
+from typing import Callable
 
 from jinja2.environment import Environment, Template
 
+from engine.utils import ENV
 
-def load_template(path: str, env: Environment) -> Callable:
+
+def load_template(template_path: str,
+                  env: Environment=ENV,
+                  fallback_format: str="jinja") -> Callable:
     """
         Load template and pass it as named argument 'template' to function
 
@@ -13,6 +19,18 @@ def load_template(path: str, env: Environment) -> Callable:
     def decor(func: Callable) -> Callable:
         @wraps(func)
         def wrapper(*args, **kwargs) -> object:
+            path = template_path
+            if not os.path.exists(path) and not path.endswith(fallback_format):
+                logging.debug(
+                    "'%s' isn't exists! Trying to find '%s' alternative",
+                    path,
+                    fallback_format
+                )
+                path = ".".join((path, fallback_format))
+                if not os.path.exists(path):
+                    logging.error("Can't find template: '%s'!", path)
+                    raise FileNotFoundError(f"'{path}' isn't exists")
+            logging.debug("Loading template '%s'...", path)
             return func(*args, template=env.get_template(path), **kwargs)
         return wrapper
     return decor
@@ -26,11 +44,12 @@ def default_args_for_render(func: Callable) -> Callable:
     """
     @wraps(func)
     def wrapper(*args,
-                company_name: str="HSE",
+                company_name: str="HSE",  # @TODO: remove hardcoded HSE
                 file_type: str=None,
                 **kwargs) -> object:
         if not file_type:
-            file_type = func.__name__.split('_')[-1]
+            file_type = func.__name__
+            logging.debug("Assume file type is '%s'.", file_type)
         return func(
             *args,
             company_name=company_name,
