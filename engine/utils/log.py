f8b4f94a4f06563d27f5d45a02236dd36aed325f
Alex <eshobee@gmail.com>
2018-01-28 16:18:13 +0300

    [engine] update utils.log: add logger as kwarg, add enable_logging_to_file

--- engine/utils/log.py
+++ engine/utils/log.py
@@ -1,9 +1,12 @@
 """ Logging for workflow (mainly functions) """
 
 import logging
+import os
 import traceback
 from collections import Callable
 from functools import wraps
+from logging import RootLogger
+from logging.handlers import RotatingFileHandler
 
 
 class Log:
@@ -16,25 +19,41 @@ class Log:
     def __init__(self,
                  case_name: str,
                  level: int=logging.DEBUG,
+                 logger: RootLogger=None,
                  **kwargs):
         self._name = case_name
         self._level = level
+        self._logger = logger if logger is not None else None
         self._enter_msg = kwargs.pop("enter_msg", "ENTERING::")
         self._exit_msg = kwargs.pop("exit_msg", "EXITING::")
         self._exc_msg = kwargs.pop("exc_msg", "")
         self._silent = kwargs.pop("silent", False)
 
+    def _log(self, *message, level: int=None) -> None:
+        """ log message """
+        if level is None:
+            level = self._level
+
+        if level == logging.ERROR:
+            if self._logger:
+                self._logger.error(*message)
+            else:
+                logging.error(*message)
+        elif level > logging.DEBUG:
+            if self._logger:
+                self._logger.info(*message)
+            else:
+                logging.info(*message)
+        else:
+            if self._logger:
+                self._logger.debug(*message)
+            else:
+                logging.debug(*message)
+
     def __enter__(self) -> object:
         """ Returns it's logger instance of self if silent """
-        if self._silent:
-            return self
-
-        message = ("%s%s", self._enter_msg, self._name)
-        if self._level > logging.DEBUG:
-            logging.info(*message)
-        else:
-            logging.debug(*message)
-
+        if not self._silent:
+            self._log("%s%s", self._enter_msg, self._name)
         return self
 
     def __exit__(self,
@@ -43,7 +62,7 @@ class Log:
                  tb: object=None) -> None:
         """ Handling exceptions (log them) """
         if exc_type is not None:
-            logging.error(
+            self._log(
                 "%s\n%s%s: %s\n",
                 self._exc_msg,
                 "\n".join([s.strip(r"\n") for s in traceback.format_tb(tb)]),
@@ -51,14 +70,8 @@ class Log:
                 exc_val
             )
 
-        if self._silent:
-            return
-
-        message = ("%s%s", self._exit_msg, self._name)
-        if self._level > logging.DEBUG:
-            logging.info(*message)
-        else:
-            logging.debug(*message)
+        if not self._silent:
+            self._log("%s%s", self._exit_msg, self._name)
 
 
 class _FuncLog(Log):
@@ -66,12 +79,14 @@ class _FuncLog(Log):
 
     def __init__(self,
                  case_name: str,
-                 level: int=logging.DEBUG):
+                 level: int=logging.DEBUG,
+                 logger: RootLogger=None):
         super(_FuncLog, self).__init__(
             case_name=case_name,
             level=level,
             enter_msg="===> ",
-            exit_msg="<--- "
+            exit_msg="<--- ",
+            logger=logger
         )
 
 
@@ -79,7 +94,8 @@ def log(log_result: bool=True,
         log_args: bool=True,
         log_kwargs: bool=True,
         name: str=None,
-        level: int=logging.DEBUG) -> Callable:
+        level: int=logging.DEBUG,
+        logger: RootLogger=None) -> Callable:
     """ Returns decorator for logging function/method behavior """
     def decor(func: Callable) -> Callable:
         func_name = name
@@ -88,7 +104,7 @@ def log(log_result: bool=True,
 
         @wraps(func)
         def wrapper(*args, **kwargs) -> object:
-            with _FuncLog(func_name, level):
+            with _FuncLog(func_name, level, logger=logger):
                 if log_args:
                     for arg in args:
                         logging.debug("()::%s", arg)
@@ -102,3 +118,32 @@ def log(log_result: bool=True,
             return result
         return wrapper
     return decor
+
+
+def enable_logging_to_file(path: str="logs",
+                           file: str="app.log",
+                           max_bytes: int=10240,
+                           backup_count: int=10,
+                           fmt: str="%(asctime)s [%(levelname)s]: "
+                                    "%(message)s [%(pathname)s:%(lineno)d]",
+                           level: int=logging.INFO,
+                           logger: RootLogger=None,
+                           **kwargs) -> RootLogger:
+    if not os.path.exists(path):
+        logging.debug("Create path for logging: '%s'.", path)
+        os.mkdir(path)
+
+    file_handler = RotatingFileHandler(
+        os.path.join(path, file),
+        maxBytes=max_bytes,
+        backupCount=backup_count
+    )
+    file_handler.setFormatter(logging.Formatter(fmt))
+    file_handler.setLevel(level)
+
+    if logger is None:
+        logger = logging.getLogger(kwargs.get("name", "$global"))
+    logger.addHandler(file_handler)
+
+    logger.info("Initial log message.")
+    return logger
