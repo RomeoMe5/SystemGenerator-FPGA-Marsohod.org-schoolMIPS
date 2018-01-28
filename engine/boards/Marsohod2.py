1a950fa0917f0543be88b9ed36b95c9a3e9e7c5f
Alex <eshobee@gmail.com>
2018-01-28 21:00:54 +0300

    [engine] update M2 prototype

--- engine/boards/Marsohod2.py
+++ engine/boards/Marsohod2.py
@@ -1,39 +1,55 @@
-import json
+import logging
 import os
 
-from engine import ROOT_DIR
+from engine.utils.load import get_static_path, load_json, load_plain_text
+from engine.utils.prepare import create_dirs
 from engine.utils.render import Render
 
-M2_STATIC_PATH = os.path.join(ROOT_DIR, "static", "m2.json")
+try:
+    from configs.engine import CONFIG
+except ImportError as err:
+    logging.warning("Can't import configuration!\n%s", err)
+    from engine.utils import Config as CONFIG
 
 
 class Marsohod2(Render):
-    def __init__(self,
-                 path: str=M2_STATIC_PATH,
-                 encoding: str="ascii",
-                 errors: str="backslashreplace") -> None:
-        self._content = None
-        with open(path, 'r', encoding=encoding, errors=errors) as fin:
-            self._content = json.load(fin)
+    def __init__(self) -> None:
+        self.path = get_static_path(
+            self.__class__.__name__,
+            CONFIG.STATIC_EXTENSION
+        )
+        self._static_content = load_json(
+            self.path,
+            encoding=CONFIG.FILE_ENCODING,
+            errors=CONFIG.FILE_ERRORS
+        )
+        self._license = load_plain_text(
+            os.path.join(CONFIG.PATHS['static'], "LICENSE"),
+            encoding=CONFIG.FILE_ENCODING,
+            errors=CONFIG.FILE_ERRORS
+        )
         super(Marsohod2, self).__init__()
 
-    # TODO: generate configuration files
-    def generate(self, project_name: str, path: str, *args, **kwargs) -> ...:
-        if os.path.exists(path):
-            raise FileExistsError("'{}' already exists".format(path))
-        else:
-            os.mkdir(path)
+    def generate(self,
+                 project_name: str,
+                 path: str=CONFIG.DESTINATION_PATH,
+                 **kwargs) -> None:
+        create_dirs(path, rewrite=False)
 
-        v = super(Marsohod2, self).v(*args, **kwargs)
-        qpf = super(Marsohod2, self).qpf(*args, **kwargs)
-        qsf = super(Marsohod2, self).qsf(*args, **kwargs)
-        sdc = super(Marsohod2, self).sdc(*args, **kwargs)
+        config_files = {
+            'v': super(Marsohod2, self).v(project_name=project_name, **kwargs),
+            'qpf': super(Marsohod2, self).qpf(**kwargs),
+            'qsf': super(Marsohod2, self).qsf(**kwargs),
+            'sdc': super(Marsohod2, self).sdc(**kwargs)
+        }
 
-        with open(os.path.join(path, "{}.v".format(project_name)), 'w') as fout:
-            fout.writelines(v)
-        with open(os.path.join(path, "{}.qpf".format(project_name)), 'w') as fout:
-            fout.writelines(v)
-        with open(os.path.join(path, "{}.v".format(project_name)), 'w') as fout:
-            fout.writelines(v)
-        with open(os.path.join(path, "{}.v".format(project_name)), 'w') as fout:
-            fout.writelines(v)
+        for extension, content in config_files.items():
+            with open(os.path.join(
+                    path, ".".join((project_name, extension))), 'w') as fout:
+                logging.info("Creating '%s'...", fout.name)
+                for line in content:
+                    fout.write(line)
+
+        with open(os.path.join(path, "LICENSE"), 'w') as fout:
+            logging.debug("Adding LICENSE: '%s'.", fout.name)
+            fout.write(self._license)
