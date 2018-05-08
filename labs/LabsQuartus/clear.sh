3854fb107389d1a2f3689aefbc0f357ffee13bd3
Alex <eshobee@gmail.com>
2018-05-09 00:27:09 +0300

    [labs] improve redundant files clearing

--- labs/LabsQuartus/clear.sh
+++ labs/LabsQuartus/clear.sh
@@ -9,29 +9,35 @@
 #    * .bsf - symbol files for block diagram/scheme (module)
 #    * .v   - Verilog HDL file
 #    * .sh  - scripting files
+#    * .hex - memory dumps
+#    * .qip - ip-functions declaration
+#    * .csv - pin assignment dumps
 
 set -e
 # set -x # debug
 # set -v # verbose output
 
-REMOVING_PATTERN="(.(vwf|bdf|qpf|qsf|v|sh|bsf)|^(.|..)$)"
+FILES_TO_EXCLUDE_PATTERN=".(vwf|bdf|qpf|qsf|v|sh|bsf|hex|qip|csv)"
+# add '..' and '.' special folders
+FILES_TO_EXCLUDE_PATTERN="($FILES_TO_EXCLUDE_PATTERN|^(.|..))$"
 
 function show_help {
     echo "remove extra files left from Quartus II exectution"
     echo
-    echo "Do not remove: '$REMOVING_PATTERN' files"
+    echo "Do not remove: '$FILES_TO_EXCLUDE_PATTERN' files"
     echo "clear.sh [-l]"
-    echo "    -l - remove files from current directory"
+    echo "    -l - remove files from current directory (local)"
     echo
 }
 
 function remove_files {
-    for file in `find . -maxdepth 1 | grep -vE "$REMOVING_PATTERN"`; do
+    for file in `find . -maxdepth 1 | grep -vE "$FILES_TO_EXCLUDE_PATTERN"`; do
         rm -r $file  # also remove folders
     done
 }
 
 for arg in $*; do
+    # TODO: use 'in' keyword
     if [ "$arg" = "-h" -o "$arg" = "-H" -o "$arg" = "--help" -o "$arg" = "/?" ]; then
         show_help
         exit
@@ -47,6 +53,7 @@ for arg in $*; do
 done
 
 for dir in `ls -d */`; do
+    # remove files inside dir (non-local)
     cd $dir
     remove_files
     cd ..
