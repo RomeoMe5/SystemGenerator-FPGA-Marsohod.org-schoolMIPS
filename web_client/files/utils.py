e4a6d9513b1742dafb72293f4936aea76b059b7f
Alex <eshobee@gmail.com>
2018-06-11 08:02:56 +0300

    [web] add ability to generate file tokens without timeout

--- web_client/files/utils.py
+++ web_client/files/utils.py
@@ -8,13 +8,11 @@ def encode_to_token(data: str,
                     expires_in: float or int=60 * 60 * 24,
                     encoding: str="utf-8") -> str:
     """ Generate time-expiring token for general proposes. """
+    payload = {'data': data}
+    if expires_in:
+        payload['exp'] = time() + expires_in
     return jwt.encode(
-        {
-            'data': data,
-            'exp': time() + expires_in
-        },
-        current_app.config['SECRET_KEY'],
-        algorithm="HS256"
+        payload, current_app.config['SECRET_KEY'], algorithm="HS256"
     ).decode(encoding)
 
 
@@ -22,9 +20,7 @@ def decode_token(token: str or bytes) -> object or None:
     """ Get data from token if it's correct. """
     try:
         return jwt.decode(
-            token,
-            current_app.config['SECRET_KEY'],
-            algorithms=['HS256']
+            token, current_app.config['SECRET_KEY'], algorithms=['HS256']
         )['data']
     except BaseException as err:
         current_app.logger.info("Invalid token:\n%s", err)
