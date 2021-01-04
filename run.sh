8f40248a6f25ad7aa13b8622a62614509d707a1f
hell03end <hell03end@outlook.com>
2021-01-05 02:52:49 +0300

    Add testing to run.sh

--- run.sh
+++ run.sh
@@ -7,5 +7,9 @@ source .venv/bin/activate
 # Install dependencies
 sudo pip3 install --no-cache -r requirements.txt
 
+# Run tests
+sudo pip3 install -U pytest
+pytest
+
 # Run web client application in debug
 python3 web_client.py -d
