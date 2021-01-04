6cedcbc96df59c6a9854d9d02df39cd5ebd73498
hell03end <hell03end@outlook.com>
2021-01-05 02:25:38 +0300

    Fix prepare.sh (app entrypoint)

--- tools/prepare.sh
+++ tools/prepare.sh
@@ -1,38 +1,15 @@
-# ===== Create virtual environment =====
+# Create virtual environment
 sudo python3 -m venv .venv
 
-# ===== Activate environment =====
+# Activate environment
 source .venv/bin/activate
 
-# ===== Install dependencies =====
-# Dev:
-sudo pip3 install --no-cache -r requirements.txt # contains web client dependencies
-sudo pip3 install --no-cache -r engine/requirements.txt # engine
-sudo pip3 install --no-cache -r tests/requirements.txt # tests
+# Install dependencies
+sudo pip3 install --no-cache -r requirements.txt
 
-# ===== Set environment variables =====
-# export FLASK_APP=app.py
-export FLASK_DEBUG=1
-export STATIC_PATH=.gen
+# Set environment variables
+export FLASK_APP=api_client.py
+export FLASK_ENV=development
 
-mkdir $STATIC_PATH
-
-mkdir configs
-
-# ===== Run application =====
-# WEB:
-flask db init
-flask db migrate -m "initial"
-flask db upgrade
-
-# if no translations exists
-flask translate init ru
-# extract strings to translate
-flask translate update
-# make translations ready to use for app
-flask translate compile
-
-# add existing posts to database
-python3 web_add_existing_posts.py
-
+# Run application
 flask run --with-threads
